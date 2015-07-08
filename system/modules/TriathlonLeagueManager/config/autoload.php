9fca80992adf937d4371762fa93d3a5a635fcc81
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-07-08 11:24:50 +0200

    Defining more detailed namespace

--- system/modules/TriathlonLeagueManager/config/autoload.php
+++ system/modules/TriathlonLeagueManager/config/autoload.php
@@ -16,7 +16,7 @@
  */
 ClassLoader::addNamespaces(array
 (
-	'TriathlonLeagueManager',
+	'CliffParnitzky\Contao\TriathlonLeagueManager',
 ));
 
 
@@ -26,17 +26,17 @@ ClassLoader::addNamespaces(array
 ClassLoader::addClasses(array
 (
 	// Classes
-	'TriathlonLeagueManager\TriathlonLeagueManagerHelper'       => 'system/modules/TriathlonLeagueManager/classes/TriathlonLeagueManagerHelper.php',
+	'CliffParnitzky\Contao\TriathlonLeagueManager\TriathlonLeagueManagerHelper'       => 'system/modules/TriathlonLeagueManager/classes/TriathlonLeagueManagerHelper.php',
 
 	// Elements
-	'TriathlonLeagueManager\ContentTriathlonLeagueManagerTable' => 'system/modules/TriathlonLeagueManager/elements/ContentTriathlonLeagueManagerTable.php',
+	'CliffParnitzky\Contao\TriathlonLeagueManager\ContentTriathlonLeagueManagerTable' => 'system/modules/TriathlonLeagueManager/elements/ContentTriathlonLeagueManagerTable.php',
 
 	// Models
-	'TriathlonLeagueManager\TriathlonLeagueTablesModel'         => 'system/modules/TriathlonLeagueManager/models/TriathlonLeagueTablesModel.php',
-	'TriathlonLeagueManager\TriathlonLeagueTeamsModel'          => 'system/modules/TriathlonLeagueManager/models/TriathlonLeagueTeamsModel.php',
+	'CliffParnitzky\Contao\TriathlonLeagueManager\TriathlonLeagueTablesModel'         => 'system/modules/TriathlonLeagueManager/models/TriathlonLeagueTablesModel.php',
+	'CliffParnitzky\Contao\TriathlonLeagueManager\TriathlonLeagueTeamsModel'          => 'system/modules/TriathlonLeagueManager/models/TriathlonLeagueTeamsModel.php',
 
 	// Modules
-	'TriathlonLeagueManager\ModuleTriathlonLeagueManagerTable'  => 'system/modules/TriathlonLeagueManager/modules/ModuleTriathlonLeagueManagerTable.php',
+	'CliffParnitzky\Contao\TriathlonLeagueManager\ModuleTriathlonLeagueManagerTable'  => 'system/modules/TriathlonLeagueManager/modules/ModuleTriathlonLeagueManagerTable.php',
 ));
 
 
--- system/modules/TriathlonLeagueManager/modules/ModuleTriathlonLeagueManagerTable.php
+++ system/modules/TriathlonLeagueManager/modules/ModuleTriathlonLeagueManagerTable.php
@@ -30,7 +30,7 @@
 /**
  * Run in a custom namespace, so the class can be replaced
  */
-namespace TriathlonLeagueManager;
+namespace CliffParnitzky\Contao\TriathlonLeagueManager;
 
 /**
  * Class ModuleTriathlonLeagueManagerTable
